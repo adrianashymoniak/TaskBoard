cea41209650c3636fecd7c12125ded31352f8485
Adriana <adrianashymoniak@gmail.com>
2018-06-10 22:07:08 +0300

    update base settings

--- task_board_application/settings/base.py
+++ task_board_application/settings/base.py
@@ -129,5 +129,5 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
