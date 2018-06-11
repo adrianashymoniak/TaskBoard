6c97c8fe82942e8032d6f6b4217b03ea795b5950
Adriana <adrianashymoniak@gmail.com>
2018-06-11 09:01:20 +0300

    updated production env

--- task_board_application/settings/production.py
+++ task_board_application/settings/production.py
@@ -2,9 +2,9 @@ from .base import *
 import dj_database_url
 
 ENVIRONMENT = 'production'
+DEBUG = False
 ALLOWED_HOSTS = ['.herokuapp.com']
 SECRET_KEY = 'SECRET_KEY'
-DEBUG = True
 DATABASES = {
     'default': dj_database_url.config(
         default='DATABASE_URL'
